557958389491c340e5b4e8569d7a6db6e27d3c9d
BmmNdr <abiemmibi@gmail.com>
2024-04-29 08:28:19 +0200

    Updated DB

--- Digiboard/Classificazione Circolari/app.py
+++ Digiboard/Classificazione Circolari/app.py
@@ -15,15 +15,16 @@ def getClassesCSV():
 
 basicPrompt = "You are a school assistant responsible for helping users find which classes should read the circular they send you. Your response should be in CSV format containing only the classes, separated by semicolons, and no additional characters or spaces. Choose the classes only from the following list; do not add anything not included:\n"
 basicPrompt += getClassesCSV()
+
 basicPrompt += "\nIf the circular shoud be read by the classes of a certain year, but with no specification on the letter, answer using the year.The circular is as follows:\n"
 
-print(basicPrompt)
-
-def main():
-    genai.configure(api_key="")
+def classify(circular):
+    genai.configure(api_key="AIzaSyDH5t5W0fiItfzIOomAvbPVgNWIQozki6U")
     
     model = genai.GenerativeModel('gemini-pro')
     
-    response = model.generate_content(string)
+    prompt = basicPrompt + circular
+    
+    response = model.generate_content(prompt)
     
     print(response.text)
